7e7ec8ec0730ae471d4398f9140bbfacd74b1640
zackynson <crys.chb@hotmail.com>
2020-02-03 09:53:03 -0300

    appointment model created

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-import multerConfig from './config/multer';
+import ProviderController from './app/controllers/ProviderController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -14,8 +17,9 @@ routes.post('/sessions', SessionController.store);
 
 // rotas abaixo usarao o middleware
 routes.use(authMiddleware);
-routes.put('/users', UserController.update);
 
+routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
