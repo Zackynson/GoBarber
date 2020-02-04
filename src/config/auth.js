3e0bc46e6cb4be0f35156d82af2448a4f83eb99a
zackynson <crys.chb@hotmail.com>
2020-02-04 14:56:04 -0300

    enviroment variables setup

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '69413ba6058246811b3273bd403ad3f1575c46a65b1925116a61e6aefddcd5f0',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0eb08d185aaa04',
-    pass: '1209604e6060b5',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
